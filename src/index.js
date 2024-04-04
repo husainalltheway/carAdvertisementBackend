22df9f28c32ab6a088a61e04dddb52052ce9a3d1
husainkhalid <khalid.husain@tecdune.com>
2024-04-04 18:26:38 +0530

    changes in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 // require ('dotenv').config({path: './env'})
+import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 
 dotenv.config({
     path: './env'
 })
+const app = express();
 connectDB()
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
