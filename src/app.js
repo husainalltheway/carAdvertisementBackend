3202790c92d86da4cdea196978067e58610e2d81
husainkhalid <khalid.husain@tecdune.com>
2024-04-16 15:09:14 +0530

    created routes and imported it in files for register user

--- src/app.js
+++ src/app.js
@@ -13,4 +13,10 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// import Routes
+import userRouter from "./routes/userRoutes.js"
+
+// declare routes
+app.use("/api/v1/users", userRouter)
+
 export { app }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 // require ('dotenv').config({path: './env'})
-import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import {app} from "./app.js";
 
 dotenv.config({
     path: './env'
 })
-const app = express();
+
 connectDB()
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.timeLog(`Server is running at port : ${process.env.PORT}`)
+        console.log(`Server is running at port : ${process.env.PORT}`)
     })
 })
 .catch((err) => {
