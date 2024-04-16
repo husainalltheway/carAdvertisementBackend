73ab62ee4fa0a8e6c5d664daf645a6c1ddce0545
husainkhalid <khalid.husain@tecdune.com>
2024-04-16 18:15:16 +0530

    refinements

--- src/constants/databaseConstants/databaseConstants.js
+++ src/constants/databaseConstants/databaseConstants.js
@@ -1,5 +1,4 @@
 /* -------------->> DATABSE COLLECTION NAMES <<-------------- */
-
 export const CLIENT_DETAILS = 'clientDetails'
 export const CAR_DETAILS = 'carDetails'
 export const ADVERTISEMENT_DETAILS = 'addDetails'
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,17 +6,14 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 const registerUser = asyncHandler(async (req, res, next) => {
   const { name, userName, email, phoneNumber, location, password } = req.body;
 
-  if (
-    [name, userName, email, phoneNumber, location, password].some(
-      (field) => field?.trim() === ""
-    )
-  ) {
+  if ([name, userName, email, phoneNumber, location, password].some((field) => field?.trim() === "")) {
     throw new ApiError(400, "All fields are required");
   }
   const existedUser = Client.findOne({
     $or: [{ email }, { userName }],
   });
   if (existedUser) {
+    console.log('existedUser: ', existedUser);
     throw new ApiError(409, "USER ALREADY EXIST");
   }
   const user = await Client.create({
@@ -34,11 +31,7 @@ const registerUser = asyncHandler(async (req, res, next) => {
     throw new ApiError(500, "Something well wrong while registring user")
   }
   return res.status(201).json(
-    new ApiResponse(
-      200,
-      createdUser,
-      "USER CREATED !!"
-    )
+    new ApiResponse(200, createdUser,"USER CREATED !!")
   )
 });
 
