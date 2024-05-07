a97845fa6c4a65436128b42a542891a917855451
husainkhalid <khalid.husain@tecdune.com>
2024-05-07 16:25:22 +0530

    update userController.js and userRoutes.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,7 @@
-import asyncHandler from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken"
 import { ApiError } from "../utils/ApiErrors.js";
 import { Client } from "../models/clientModel.js";
+import asyncHandler from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 // Funtion to generate access and refresh token 
@@ -134,5 +135,50 @@ const logoutUser = asyncHandler(async(req, res) => {
 
 })
 
+const refreshAccessToken = asyncHandler(async(req, res) => {
+  const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
-export { registerUser, loginUser, logoutUser };
+  if(!incomingRefreshToken) {
+    throw new ApiError(401, "Unauthorized Request")
+  }
+
+  try {
+    const decodedToken = jwt.verify(incomingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    )
+  
+    const user = await Client.findById(decodedToken?._id)
+  
+    if(!user) {
+      throw new ApiError(401, "Invalid refresh token")
+    }
+  
+    if (incomingRefreshToken !== user?.refreshToken) {
+      throw new ApiError(401, "Refresh token is expird or used")
+    }
+  
+    const options = {
+      httpOnly: true,
+      secure: true
+    }
+  
+    const {accessToken, newRefreshToken} = await generateAccessAndRefreshTokens(user._id)
+  
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", newRefreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {accessToken, refreshToken: newRefreshToken},
+        "Access Token Refreshed"
+      )
+    )
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid refresh token")
+  }
+})
+
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/userController.js";
+import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/userController.js";
 import { uploadAdvertisementFile } from "../controllers/fileController.js";
 import { upload } from "../middlewares/multerMiddleware.js";
 import { verifyJWT } from "../middlewares/authentication.js";
@@ -13,5 +13,6 @@ router.route("/login").post(loginUser)
 
 // secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
 
 export default router;
