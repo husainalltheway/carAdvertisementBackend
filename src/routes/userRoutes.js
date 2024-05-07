1842196ba2aca5221be56a94629cf3275445285b
husainkhalid <khalid.husain@tecdune.com>
2024-05-07 11:49:50 +0530

    user log out api created

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/userController.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/userController.js";
 import { uploadAdvertisementFile } from "../controllers/fileController.js";
 import { upload } from "../middlewares/multerMiddleware.js";
+import { verifyJWT } from "../middlewares/authentication.js";
 
 const router = Router()
 
 router.route("/register").post(registerUser)
 router.route("/uploadAdvertisementFile").post(upload.fields([{name: 'advertisementFile', maxCount: 1}]),uploadAdvertisementFile)
+router.route("/login").post(loginUser)
+
+
+// secured routes
+router.route("logout").post(verifyJWT, logoutUser)
 
 export default router;
